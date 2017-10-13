d3fef535e97a8e4012c981a7f24819a11299140d
leota <leotagit@gmail.com>
2017-10-13 16:21:03 +0200

    FIX: App-routing module moved into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material.module';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
 
